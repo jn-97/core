784ecd3fee45b903bced77eccc4aef36d01f3e82
jn-97 <kjo5395@gmail.com>
2023-06-19 16:32:32 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -15,17 +15,16 @@ public class OrderServiceImpl implements OrderService {
         this.discountPolicy = discountPolicy;
     }
 
-    /**
-     * 클래스 의존 관계를 분석해 보면, 추상(인터페이스) 뿐만 아니라 구체(구현) 클래스에도 의존하고 있다.
-     * 추상(인터페이스) 의존 : DiscountPolicy | 구체(구현) 클래스 : FixDiscountPolicy, RateDiscountPolicy
-     * FixDiscountPolicy -> RateDiscountPolicy 구현 클래스 변경 => DIP와 OCP 위반
-     */
-
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
        Member member = memberRepository.findById(memberId);
        int discountPrice = discountPolicy.discount(member, itemPrice);
 
        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }

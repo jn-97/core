687d7d8d983193c6a4fc53b64c028aea64cf8fca
jn-97 <kjo5395@gmail.com>
2023-06-23 15:25:18 +0900

    @Autowired 필드 명, @Qualifier, @Primary

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,11 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary; // 조회 빈이 중복일 때 우선으로!
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;

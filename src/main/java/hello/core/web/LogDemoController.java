e228b23a6041dff1356f4c9cd65f2d7684e1354b
jn-97 <kjo5395@gmail.com>
2023-06-27 14:39:08 +0900

    스코프와 Provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,7 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,14 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-
-    @Autowired
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        String requestURL = request.getRequestURI().toString();
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
@@ -29,3 +28,11 @@ public class LogDemoController {
         return "OK";
     }
 }
+
+/*
+[3dc81f1c-ee66-4ccb-9d46-27f67072757f] request scope bean create:hello.core.common.MyLogger@331f9bc1
+[3dc81f1c-ee66-4ccb-9d46-27f67072757f][http://localhost:8080/log-demo] controller test
+[3dc81f1c-ee66-4ccb-9d46-27f67072757f][http://localhost:8080/log-demo] service id = testId
+[3dc81f1c-ee66-4ccb-9d46-27f67072757f] request scope bean close:hello.core.common.MyLogger@331f9bc1
+*/
+
